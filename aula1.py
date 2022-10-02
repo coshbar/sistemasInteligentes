fd23bfbd5a098e8ddffc80da5264f44fe8f0923f
coshbar <43322369+coshbar@users.noreply.github.com>
2022-10-02 19:25:54 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+from sklearn import preprocessing
+from pickle import dump
+data = pd.read_csv('/content/BostonHousing.csv', sep = ';')
+#normalizacao
+data_normalized = (data - data.min()) / (data.max() - data.min())
+#padronizacao
+data_patterned = (data - data.mean()) / data.std()
+#normalizacao e modelo
+normalizer = preprocessing.MinMaxScaler()
+normalized_data_model = normalizer.fit(data)
+#salvar
+dump(normalized_data_model, open('normalized_data_model', 'wb')
+)
